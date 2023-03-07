ce4e9fe28701506bc0025c45766c14a0ac06ec89
zamroczynski <damianzamroczynski@gmail.com>
2023-03-07 10:21:35 +0100

    add login button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MyLogo from './components/MyLogo';
-import SelectLanguage from './components/SelectLanguage';
-import Login from './components/Login';
-import AddWordForm from './components/AddWordForm';
-import { useState } from 'react';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyLogo from "./components/MyLogo";
+import SelectLanguage from "./components/SelectLanguage";
+import Login from "./components/Login";
+import AddWordForm from "./components/AddWordForm";
+import { useState } from "react";
 
 function App() {
   const [token, setToken] = useState("");
   const [isLogged, setIsLogged] = useState(false);
   const [languages, setLanguages] = useState([]);
+  const [showLoginForm, setShowLoginForm] = useState(false);
 
   function handleLoginData(data) {
     setToken(data.token);
@@ -21,14 +21,37 @@ function App() {
     setLanguages(data);
   }
 
+  function handleButtonLogin() {
+    setShowLoginForm(true);
+  }
+
   return (
-    <div className='container'>
-      <div className='row'><MyLogo /></div>
-      <div className='row'><Login onDataReceived={handleLoginData} isLogged={isLogged} /></div>
-      <div className='row'><SelectLanguage onDataReceived={handleLanguagesData} /></div>
-      <div className='row'>{isLogged && <AddWordForm token={token} languages={languages} />}</div>
+    <div className="container">
+      <div className="row">
+        <MyLogo />
+      </div>
+      <div className="row">
+        {!showLoginForm && (
+          <button
+            className="btn btn-success btn-sm text-center"
+            style={{width: "100px"}}
+            onClick={handleButtonLogin}
+          >
+            Logowanie
+          </button>
+        )}
+        {showLoginForm && (
+          <Login onDataReceived={handleLoginData} isLogged={isLogged} />
+        )}
+      </div>
+      <div className="row">
+        <SelectLanguage onDataReceived={handleLanguagesData} />
+      </div>
+      <div className="row">
+        {isLogged && <AddWordForm token={token} languages={languages} />}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
